c7eb8ffd49024f1f9fd0d85c7ad886433d0a2807
Roman Nikitenko <Lomkaaa@ukr.net>
2024-01-31 23:54:11 +0200

    Added hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
