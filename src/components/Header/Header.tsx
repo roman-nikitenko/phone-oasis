bcf8d080346a4d11a45f7e1e261086c184e9354a
Roman Nikitenko <Lomkaaa@ukr.net>
2024-02-01 16:12:34 +0200

    test

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header: React.FC = () => {
   return (
     <div className="Navigation">
-      <h1 className="text-Red font-bold ">Header</h1>
+      <h1 className="text-Green font-bold ">Header</h1>
     </div>
   );
 };
