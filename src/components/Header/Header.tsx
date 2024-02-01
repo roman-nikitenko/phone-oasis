1f99430ddd305efc8d8df90d2713becd9dd8855c
Roman Nikitenko <Lomkaaa@ukr.net>
2024-02-01 15:04:41 +0200

    test develop ED-2a

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 export const Header: React.FC = () => {
   return (
     <div className="Navigation">
-      <h1 className="text-orange-500">Navigation</h1>
-      <h2 className="text-orange-700">Test deploy develop</h2>
-      <h2 className="text-orange-700">Test deploy develop</h2>
-      <h2 className="text-orange-700">Test deploy develop</h2>
-      <h2 className="text-orange-700">Test deploy develop</h2>
+
     </div>
   );
 };
