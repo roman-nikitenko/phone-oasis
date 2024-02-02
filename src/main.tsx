c491c4629faccba1e559268fad72d85e24668b25
Roman Nikitenko <Lomkaaa@ukr.net>
2024-02-02 15:52:18 +0200

    Change rout method

--- src/main.tsx
+++ src/main.tsx
@@ -1,78 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { RouterProvider, createHashRouter, Navigate } from 'react-router-dom';
-import {HomePage} from "./pages/HomePage.tsx";
-import {PhonesPage} from "./pages/PhonesPage.tsx";
-import {TabletsPage} from "./pages/TabletsPage.tsx";
-import {AccessoriesPage} from "./pages/AccessoriesPage.tsx";
-import {ProductItemPage} from "./pages/ProductItemPage.tsx";
-import {FavouritesPage} from "./pages/FavouritesPage.tsx";
-import {CartPage} from "./pages/CartPage.tsx";
+import { HashRouter, Route, Routes } from 'react-router-dom';
+// import {HomePage} from "./pages/HomePage.tsx";
+// import {PhonesPage} from "./pages/PhonesPage.tsx";
+// import {TabletsPage} from "./pages/TabletsPage.tsx";
+// import {AccessoriesPage} from "./pages/AccessoriesPage.tsx";
+// import {ProductItemPage} from "./pages/ProductItemPage.tsx";
+// import {FavouritesPage} from "./pages/FavouritesPage.tsx";
+// import {CartPage} from "./pages/CartPage.tsx";
 // import { ErrorPage } from './pages/ErrorPage.tsx';
-import { ContactsPage } from './pages/ContactsPage.tsx';
-import { GithubPage } from './pages/GithubPage.tsx';
-import { RightsPage } from './pages/RightsPage.tsx';
+// import { ContactsPage } from './pages/ContactsPage.tsx';
+// import { GithubPage } from './pages/GithubPage.tsx';
+// import { RightsPage } from './pages/RightsPage.tsx';
 import './index.css';
+import { HomePage } from './pages/HomePage.tsx';
+import { PhonesPage } from './pages/PhonesPage.tsx';
+import { TabletsPage } from './pages/TabletsPage.tsx';
+import { AccessoriesPage } from './pages/AccessoriesPage.tsx';
 
-const router = createHashRouter([
-  {
-    path: '/',
-    element: <App />,
-    // errorElement: <ErrorPage />,
-    children: [
-      {index: true, element: <Navigate to="/home" replace /> },
-      {
-        path: 'home',
-        element: <HomePage />,
-        index: true,
-      },
-      {
-        path: 'phones',
-        element: <PhonesPage />,
-      },
-      {
-        path: 'phones/:id',
-        element: <ProductItemPage />,
-      },
-      {
-        path: 'tablets',
-        element: <TabletsPage />,
-      },
-      {
-        path: 'tablets/:id',
-        element: <ProductItemPage />,
-      },
-      {
-        path: 'accessories',
-        element: <AccessoriesPage />,
-      },
-      {
-        path: 'favourites',
-        element: <FavouritesPage />,
-      },
-      {
-        path: 'cart',
-        element: <CartPage />,
-      },
-      {
-        path: 'contacts',
-        element: <ContactsPage />,
-      },
-      {
-        path: 'github',
-        element: <GithubPage />,
-      },
-      {
-        path: 'rights',
-        element: <RightsPage />,
-      },
-    ],
-  },
-])
+// const router = createHashRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {index: true, element: <Navigate to="/home" replace /> },
+//       {
+//         path: 'home',
+//         element: <HomePage />,
+//         index: true,
+//       },
+//       {
+//         path: 'phones',
+//         element: <PhonesPage />,
+//       },
+//       {
+//         path: 'phones/:id',
+//         element: <ProductItemPage />,
+//       },
+//       {
+//         path: 'tablets',
+//         element: <TabletsPage />,
+//       },
+//       {
+//         path: 'tablets/:id',
+//         element: <ProductItemPage />,
+//       },
+//       {
+//         path: 'accessories',
+//         element: <AccessoriesPage />,
+//       },
+//       {
+//         path: 'favourites',
+//         element: <FavouritesPage />,
+//       },
+//       {
+//         path: 'cart',
+//         element: <CartPage />,
+//       },
+//       {
+//         path: 'contacts',
+//         element: <ContactsPage />,
+//       },
+//       {
+//         path: 'github',
+//         element: <GithubPage />,
+//       },
+//       {
+//         path: 'rights',
+//         element: <RightsPage />,
+//       },
+//     ],
+//   },
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}  >
+          <Route path="/" element={<HomePage />} />
+          <Route path="/phones" element={<PhonesPage />} />
+          <Route path="/tablets" element={<TabletsPage />} />
+          <Route path="/accessories" element={<AccessoriesPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 )
