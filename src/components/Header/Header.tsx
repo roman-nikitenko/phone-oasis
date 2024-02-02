0261a3ddd7e5eac64602f57d6292984370e055c6
Roman Nikitenko <Lomkaaa@ukr.net>
2024-02-02 11:14:44 +0200

    added responsive Header with navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Logo } from '../Logo.tsx';
+import { Navigation } from './Navigation.tsx';
+import { Icon } from './Icon.tsx';
+import basket from '../../assets/cart.svg'
+import like from '../../assets/like.svg'
+
 
 export const Header: React.FC = () => {
   return (
-    <div className="Header">
-      <h1 className="text-orange-500">Header</h1>
+    <div className="h-[64px] flex items-center pl-[24px] justify-between relative border-b border-Elements">
+      <div className="flex gap-[64px] items-center">
+        <Logo />
+        <div className="hidden md:flex">
+          <Navigation />
+        </div>
+      </div>
+      <div className="flex">
+        <Icon image={like} />
+        <Icon image={basket} />
+      </div>
     </div>
   );
 };
---
+++ src/components/Header/Icon.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {
+  image: string,
+}
+
+export const Icon: React.FC<Props> = ({ image }) => {
+  return (
+    <div className="cursor-pointer border-l border-Elements w-[64px] h-[64px] flex justify-center items-center">
+      <img src={image} />
+    </div>
+  );
+};
