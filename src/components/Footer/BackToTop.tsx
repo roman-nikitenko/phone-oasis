16009abcf99b6139d9faaffa0747f6b95572acfc
Adriana S <adrianasuhanova@gmail.com>
2024-02-03 19:09:25 +0200

    fix arrow-up icon

--- src/components/Footer/BackToTop.tsx
+++ src/components/Footer/BackToTop.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import arrowUp from 'assets/arrow-up-darkgrey.svg';
 
 export const BackToTop: React.FC = () => {
   return (
     <div className=" items-center gap-[16px] hidden sm:flex">
       <span className="small-text text-Secondary">Back to top</span>
       <div className="flex justify-center items-center border border-Icons transition-colors hover:border-Primary cursor-pointer w-[32px] h-[32px]">
-        <img src={'src/assets/arrow-up-darkgrey.svg'} alt="arrow up icon" />
+        <img src={arrowUp} alt="arrow up icon" />
       </div>
     </div>
   );
