9cf39bbc39255fe1871e7f64b29cb1c90ee45ecb
Roman Nikitenko <Lomkaaa@ukr.net>
2024-02-04 18:23:39 +0200

    added carousel

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { CarouselHero } from '../components/CarouselHero.tsx';
+import banner from 'assets/banner.jpeg';
+import tablet from 'assets/tablet.jpg';
+import ipad from 'assets/ipad.jpg';
 
 export const HomePage: React.FC = () => {
+  const slides = [banner, tablet, ipad ]
+
   return (
-    <div className="px-[152px] pt-[40px] flex justify-center items-center">
-      <CarouselHero />
+    <div className="px-[152px] pt-[40px]">
+      <div className="flex justify-center">
+        <CarouselHero slides={slides} />
+      </div>
     </div>
   );
 };
