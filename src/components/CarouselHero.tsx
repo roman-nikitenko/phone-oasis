9c45c601fc14afbefd073edcc0b7abf5821c6a15
Roman Nikitenko <Lomkaaa@ukr.net>
2024-02-08 13:52:00 +0200

    added changes on hero banner

--- src/components/CarouselHero.tsx
+++ src/components/CarouselHero.tsx
@@ -1,51 +1,94 @@
 import React, { useState } from 'react';
-import nextIcon from 'assets/arrow-right-darkgrey.svg';
-import prevIcon from 'assets/arrow-left-darkgrey.svg';
+import { IconButton } from '@material-tailwind/react';
+import { Banner } from '../types/banner.ts';
 
 type Props = {
-  slides: string[];
-}
+  slides: Banner[];
+};
 
 export const CarouselHero: React.FC<Props> = ({ slides }) => {
-  const [current, setCurrent] = useState(0)
+  const [current, setCurrent] = useState(0);
 
-  const prev = () => setCurrent(curr => curr === 0 ? slides.length -1 : curr - 1 )
-  const next = () => setCurrent(curr => curr === slides.length -1 ? 0 : curr + 1 )
+  const prev = () => setCurrent((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
+  const next = () => setCurrent((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
+
+  //[1136px]
 
   return (
     <>
-      <div className="">
-           <div className="flex items-center gap-[16px] mb-[18px]">
-             <button
-              className="border border-Icons h-[400px] w-[32px] flex items-center justify-center"
-              onClick={prev}
+      <div className="max-w-[1136px] w-full">
+        <div className="flex items-center mb-[18px] relative ">
+          <div className=" rounded-2xl border overflow-hidden ">
+            <div
+              className="flex transition duration-700"
+              style={{ transform: `translateX(-${current * 100}%)` }}
             >
-              <img src={prevIcon}/>
-            </button>
-            <div className="w-[1040px] h-[400px] overflow-hidden ">
-              <div
-                className="flex transition duration-700"
-                style={{ transform: `translateX(-${current * 100}%)` }}
-              >
-                {slides.map(s => (
-                  <img className="h-full" src={s} />
-                ))}
-              </div>
+              {slides.map((s) => (
+                <img src={s.image} alt="banner" key={s.id} className="" />
+              ))}
             </div>
-            <button onClick={next} className="border border-Icons h-[400px] w-[32px] flex items-center justify-center" >
-              <img src={nextIcon}/>
-            </button>
           </div>
-          <div className="flex gap-[14px] justify-center">
-            {slides.map((_, i) => (
-              <div onClick={() => setCurrent(i)} className={`
-                w-[14px] h-[4px] cursor-pointer ${current === i ? 'bg-Primary' : 'bg-Elements'} 
-                transition duration-700
-              `} />
-            ))}
+          <div className="absolute inset-0 flex items-center justify-between p-4">
+            <IconButton
+              placeholder="prev button"
+              variant="text"
+              color="white"
+              size="lg"
+              onClick={prev}
+              className=""
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                />
+              </svg>
+            </IconButton>
+            <IconButton
+              placeholder="next button"
+              variant="text"
+              color="white"
+              size="lg"
+              onClick={next}
+              className=""
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                />
+              </svg>
+            </IconButton>
           </div>
         </div>
+        <div className="flex gap-[14px] justify-center">
+          {slides.map((_, i) => (
+            <div
+              onClick={() => setCurrent(i)}
+              className={`
+              w-[14px] h-[4px] cursor-pointer ${current === i ? 'bg-Primary' : 'bg-Elements'} 
+              transition duration-700
+            `}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
-
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { CarouselHero } from 'components/CarouselHero.tsx';
 import { Suggestions } from 'components/Suggestions/Suggestions.tsx';
 import { Categories } from 'components/Categories/Categories.tsx';
-import banner from 'assets/banner.jpeg';
-import tablet from 'assets/tablet.jpg';
-import ipad from 'assets/ipad.jpg';
+import mack from 'assets/banner/mack.jpg';
+import tablet from 'assets/banner/stufs.jpg';
+import ipad from 'assets/banner/iphones.jpg';
 import { useAppSelector } from '../hooks/hooks.ts';
 
 export const HomePage: React.FC = () => {
   const phones = useAppSelector((state) => state.phones);
-  const slides = [banner, tablet, ipad];
+  const slides = [
+    { id: 1, image: mack },
+    { id: 2, image: tablet },
+    { id: 3, image: ipad },
+  ];
 
   return (
-    <div className="sm:px-[20px] md:py-[40px] xl:px-[152px] lg:py-[80px] grid justify-items-center gap-[40px] xl:gap-[80px]">
+    <div className="sm:px-[20px] md:py-[40px] xl:px-[152px] lg:py-[80px] grid justify-items-center pt-[20px] px-[5px] gap-[40px] xl:gap-[80px]">
       <div className="flex justify-center">
         <CarouselHero slides={slides} />
       </div>
