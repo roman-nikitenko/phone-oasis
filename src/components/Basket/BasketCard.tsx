2ddd4854c7dd06ef0e6f96ffb8fbff8271da1871
Roman Nikitenko <Lomkaaa@ukr.net>
2024-02-15 13:14:09 +0200

    Added some text if basket is empty!

--- src/components/Basket/BasketCard.tsx
+++ src/components/Basket/BasketCard.tsx
@@ -42,7 +42,7 @@ export const BasketCard: React.FC<Props> = ({ phone }) => {
   };
 
   return (
-    <div className="md:flex border border-Elements relative md:justify-between p-[16px] md:p-[31px]">
+    <div className="md:flex lg:w-[752px] border border-Elements relative md:justify-between p-[16px] md:p-[31px]">
       <div className="flex mb-6 md:mb-0 items-center gap-[16px]">
         <img
           onClick={deleteItem}
@@ -61,7 +61,7 @@ export const BasketCard: React.FC<Props> = ({ phone }) => {
           alt="cancel"
         />
       </div>
-      <div className="flex items-center md:gap-[20px] justify-between">
+      <div className="flex items-center md:gap-[20px] lg:gap-[43px] justify-between">
         <div className="flex items-center justify-between w-[100px]">
           <button
             className="w-8 h-8 border border-Elements flex justify-center items-center"
@@ -82,32 +82,3 @@ export const BasketCard: React.FC<Props> = ({ phone }) => {
     </div>
   );
 };
-
-// <div className="border border-Elements xl:w-[752px] flex flex-col py-[31px] pl-[24px] pr-[40px]">
-//   <div className="flex items-center gap-[24px]">
-//     <img onClick={deleteItem} src={closeIcon} className="cursor-pointer" alt="cancel" />
-//     <img src={image} alt="image phone" className="w-[66px] h-[66px]" />
-//     <p className="w-[295px]">
-//       <Link to={`/detailPage/${id}`}>{title}</Link>
-//     </p>
-//   </div>
-//
-//   <div className="flex justify-between w-[200px]">
-//     <div className="flex items-center justify-between w-[100px]">
-//       <button
-//         className="w-8 h-8 border border-Elements flex justify-center items-center"
-//         onClick={() => quantityHandler(Options.MINUS)}
-//       >
-//         <img src={minusIcon} alt="button minus" />
-//       </button>
-//       <p> {quantity} </p>
-//       <button
-//         className="w-8 h-8 border border-Elements flex justify-center items-center"
-//         onClick={() => quantityHandler(Options.PLUS)}
-//       >
-//         <img src={plusIcon} alt="button minus" />
-//       </button>
-//     </div>
-//     <h2>${priceCount}</h2>
-//   </div>
-// </div>
